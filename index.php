a464f1d5443e1de6ac61b6545b845b8b55ecc820
dmtrs <tydeas.dr@gmail.com>
2013-09-20 23:41:51 +0300

    setup config

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 date_default_timezone_set('Europe/Athens');
 // change the following paths if necessary
 $yii=dirname(__FILE__).'/lib/yii/framework/yii.php';
-$config=dirname(__FILE__).'/protected/config/main.php';
 
+$base = require_once dirname(__FILE__).'/protected/config/base.php';
+$main  = require_once dirname(__FILE__).'/protected/config/main.php';
+
+$config = array_merge($base,$main);
 // remove the following lines when in production mode
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
