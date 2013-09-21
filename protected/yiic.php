9411345e7b22f60397050d62f003fe5daf2e0b9f
kalabakas <georgekyrg@gmail.com>
2013-09-21 12:29:33 +0300

    fix yiic for a working console

--- protected/yiic.php
+++ protected/yiic.php
@@ -1,7 +1,12 @@
 <?php
 
 // change the following paths if necessary
+$base  = require_once dirname(__FILE__).'/config/console.php';
+$main  = require_once dirname(__FILE__).'/config/main.php';
+
+$config = array_merge_recursive($base,$main);
+
 $yiic=dirname(__FILE__).'/../lib/yii/framework/yiic.php';
-$config=dirname(__FILE__).'/config/console.php';
+//$config=dirname(__FILE__).'/config/console.php';
 
 require_once($yiic);
