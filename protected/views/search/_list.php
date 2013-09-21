cd21cd80f0a286547d5539e5fb8cceaee1f9eb57
dmtrs <tydeas.dr@gmail.com>
2013-09-21 19:57:52 +0300

    Preparing journey

--- protected/views/search/_list.php
+++ protected/views/search/_list.php
@@ -9,13 +9,34 @@ $this->widget('zii.widgets.CListView', array(
 <?php
 $script = <<<EOF
 $('div[data-hook="search-list"]').on('click', 'input[data-hook="add-item"]', function() {
-    var frm=$(this).closest('form');
+    var btn=$(this),
+        frm=$(this).closest('form');
+
     $.ajax({
         url: frm.attr('action'),
         data: frm.serialize(),
         type: 'POST',
-        sucess: function(r) {
-            console.log(r);
+        success: function(r) {
+            if(r.added) {
+                btn.val('Remove').removeClass('btn-primary').addClass('btn-warning');
+                btn.attr('data-hook','remove-item');
+                frm.attr('action', r.action);
+            }
+        }
+    });
+}).on('click', 'input[data-hook="remove-item"]', function() {
+    var btn=$(this),
+        frm=$(this).closest('form');
+
+    $.ajax({
+        url: frm.attr('action'),
+        type: 'POST',
+        success: function(r) {
+            if(r.removed) {
+                btn.val('Add').addClass('btn-primary').removeClass('btn-warning');
+                btn.attr('data-hook','add-item');
+                frm.attr('action', r.action);
+            }
         }
     });
 });
--- protected/views/timeline/index.php
+++ protected/views/timeline/index.php
@@ -3,14 +3,16 @@ $this->breadcrumbs=array(
 	'Timelines',
 );
 
-$this->menu=array(
+/**$this->menu=array(
 	array('label'=>'Create Timeline','url'=>array('create')),
 	array('label'=>'Manage Timeline','url'=>array('admin')),
-);
+);**/
 ?>
 
 <h1>Timelines</h1>
-
+<?php
+echo CHtml::link('Create new',array('timeline/create'), array('class'=>'btn btn-primary'));
+?>
 <?php $this->widget('bootstrap.widgets.TbListView',array(
 	'dataProvider'=>$dataProvider,
 	'itemView'=>'_view',
